ac93baf2f89170710a7ed4d2b6a71f6c55319437
dddsnn <marc.lehmann@gmx.de>
2015-03-25 18:09:34 +0100

    solution to the deployment error caused by replay battle etc

--- src/cworg/data/ReplayBattle.java
+++ src/cworg/data/ReplayBattle.java
@@ -1,9 +1,20 @@
 package cworg.data;
 
 import java.io.Serializable;
+import java.time.Duration;
+import java.time.Instant;
+import java.util.Set;
 
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
 
-//@Entity
+@Entity
 public class ReplayBattle implements Serializable {
 	private static final long serialVersionUID = 1L;
 
@@ -16,31 +27,28 @@ public class ReplayBattle implements Serializable {
 		STRONGHOLD_SKIRMISH, STRONGHOLD_BATTLE
 	}
 
-//	@Id
-//	@Column(name = "ARENA_ID")
-//	private long arenaId;
-//	@Id
-//	@Column(name = "PLAYER_ID")
-//	private String playerId;
-//	@OneToOne(fetch = FetchType.LAZY)
-//	@MapsId
-//	@JoinColumn(name = "PLAYER_ID")
-//	private Player player;
-//	private BattleType battleType;
-//	private boolean lockingEnabled;
-//	private Instant arenaCreateTime;
-//	private String mapName;
-//	private Duration duration;
-//	private int winningTeam;
-//	private BattleOutcome outcome;
-//	@OneToMany(fetch = FetchType.LAZY, mappedBy = "battle",
-//			cascade = CascadeType.ALL)
-//	private Set<ReplayPlayer> team1;
-//	@OneToMany(fetch = FetchType.LAZY, mappedBy = "battle",
-//			cascade = CascadeType.ALL)
-//	private Set<ReplayPlayer> team2;
-//
-//	public ReplayBattle() {
-//	}
+	@Id
+	@Column(name = "ARENA_ID")
+	private long arenaId;
+	@OneToOne(fetch = FetchType.LAZY)
+	@Id
+	@JoinColumn(name = "PLAYER_ID")
+	private Player player;
+	private BattleType battleType;
+	private boolean lockingEnabled;
+	private Instant arenaCreateTime;
+	private String mapName;
+	private Duration duration;
+	private int winningTeam;
+	private BattleOutcome outcome;
+	@OneToMany(fetch = FetchType.LAZY, mappedBy = "battle",
+			cascade = CascadeType.ALL)
+	private Set<ReplayPlayer> team1;
+	@OneToMany(fetch = FetchType.LAZY, mappedBy = "battle",
+			cascade = CascadeType.ALL)
+	private Set<ReplayPlayer> team2;
+
+	public ReplayBattle() {
+	}
 
 }
