8bb25c2ed4558faa8987e101bcfdacb709fe1526
dddsnn <marc.lehmann@gmx.de>
2015-04-02 18:42:21 +0200

    filter to redirect users who aren't logged in away from /home/*

---
+++ src/cworg/servlets/LoggedInFilter.java
@@ -0,0 +1,42 @@
+package cworg.servlets;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import cworg.data.User;
+
+@WebFilter("/home/*")
+public class LoggedInFilter implements Filter {
+	@Override
+	public void init(FilterConfig fConfig) throws ServletException {
+		// TODO Auto-generated method stub
+	}
+
+	@Override
+	public void destroy() {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void doFilter(ServletRequest request, ServletResponse response,
+			FilterChain chain) throws IOException, ServletException {
+		HttpServletRequest req = (HttpServletRequest) request;
+		HttpServletResponse resp = (HttpServletResponse) response;
+		User user = (User) req.getSession().getAttribute("user");
+		if (user == null) {
+			// not logged in, redirect to index
+			resp.sendRedirect("/");
+		} else {
+			chain.doFilter(request, response);
+		}
+	}
+}
